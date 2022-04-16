7452ae6e566390b1eaf69f30759c145a3d81c2db
shelly.bendor <shelly.bendor@cs.huji.ac.il>
2022-04-16 19:07:30 +0300

    Updates

---
+++ precache-manifest.b621c28a403d6d1c07cd5eae9464bd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5be719ffda28e902c5c",
+    "url": "/your-humor-deploy/static/css/main.063fe5e4.chunk.css"
+  },
+  {
+    "revision": "e5be719ffda28e902c5c",
+    "url": "/your-humor-deploy/static/js/main.5ba794b7.chunk.js"
+  },
+  {
+    "revision": "8330947132ac6d1564b3",
+    "url": "/your-humor-deploy/static/js/runtime~main.8f446804.js"
+  },
+  {
+    "revision": "4da7a2a01637a940b62d",
+    "url": "/your-humor-deploy/static/js/2.15369ab4.chunk.js"
+  },
+  {
+    "revision": "9695ab0c1d8a5013f8e7",
+    "url": "/your-humor-deploy/static/js/3.a620750f.chunk.js"
+  },
+  {
+    "revision": "55661a5db1a542505a150e7dd394aee2",
+    "url": "/your-humor-deploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e02df949e0e7067ea5772ecb59ffb351.js"
+  "/your-humor-deploy/precache-manifest.b621c28a403d6d1c07cd5eae9464bd9c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/your-humor-deploy/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
