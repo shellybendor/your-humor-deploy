e9a2f5a00b5f2f08fddca6508dfa084efc69131f
shelly.bendor <shelly.bendor@cs.huji.ac.il>
2022-04-16 18:54:17 +0300

    Updates

---
+++ precache-manifest.7218a0e421830b0b4550443cdd18fc56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5be719ffda28e902c5c",
+    "url": "/static/css/main.063fe5e4.chunk.css"
+  },
+  {
+    "revision": "e5be719ffda28e902c5c",
+    "url": "/static/js/main.5ba794b7.chunk.js"
+  },
+  {
+    "revision": "6bec4cd6d7198a5f5a0e",
+    "url": "/static/js/runtime~main.213fdc25.js"
+  },
+  {
+    "revision": "2b2107403c7abf57dcce",
+    "url": "/static/js/2.71748967.chunk.js"
+  },
+  {
+    "revision": "9695ab0c1d8a5013f8e7",
+    "url": "/static/js/3.a620750f.chunk.js"
+  },
+  {
+    "revision": "29da4a2409cb6fa4381fdc52daf12ffe",
+    "url": "/index.html"
+  }
+];
